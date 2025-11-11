b675e967f6e876be9e089939de5c8201ca2b2328
novacloud06-del <novacloud06@gmail.com>
2025-11-11 21:20:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -529,8 +529,7 @@ async def connect_personal_drive(drive_id: str = "drive_1", current_user: str = 
             },
             scopes=[
                 'https://www.googleapis.com/auth/drive.file',
-                'https://www.googleapis.com/auth/userinfo.email',
-                'https://www.googleapis.com/auth/userinfo.profile',
+                'https://www.googleapis.com/auth/drive',
                 'openid'
             ]
         )
@@ -642,8 +641,7 @@ async def oauth2callback(code: Optional[str] = None, state: Optional[str] = None
             },
             scopes=[
                 'https://www.googleapis.com/auth/drive.file',
-                'https://www.googleapis.com/auth/userinfo.email',
-                'https://www.googleapis.com/auth/userinfo.profile',
+                'https://www.googleapis.com/auth/drive',
                 'openid'
             ]
         )
